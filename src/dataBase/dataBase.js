3670532dff918b957fed26e7582f5102aa7f8c03
PVARGASM1 <andrea.vargas18@hotmail.es>
2024-08-17 23:18:20 -0500

    User Crud and conexion with DB working well

--- src/dataBase/dataBase.js
+++ src/dataBase/dataBase.js
@@ -1,32 +1,24 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const urlDB = 'mongodb+srv://andreavargas18:41jAOsTPstziqbY5@cluster0.k8vw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Crear un MongoClient con un objeto MongoClientOptions para establecer la versión API
 const client = new MongoClient(urlDB, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
       deprecationErrors: true,
     }
-  });
-  
+});
 
-async function addUser(nombre, email, password) {
+async function connectDB() {
   try {
-    await client.connect();// Connect the client to the server
+    await client.connect();
+    console.log("Conectado a MongoDB");
     const db = client.db("GuardianSecurityApp");
-    const userCollection = db.collection('user');
-
-    const usuario = { nombre, email, password }
-
-    const insertUser = await userCollection.insertOne({ usuario});
-    return insertUser // Retorna el usuario insertado
-   
-  } finally {
-    // Ensures that the client will close when you finish/error
-     await client.close();
+    return db.collection('user');
+  } catch (error) {
+    console.error("Error conectando a la base de datos", error);
   }
 }
-addUser().catch(console.dir);
 
-module.exports = addUser();
+module.exports = { connectDB };
--- src/index.js
+++ src/index.js
@@ -1,54 +1,98 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient } = require('mongodb');
-const addUser = require('./dataBase/dataBase.js')
+const { connectDB } = require('./dataBase/dataBase');
+const { ObjectId } = require('mongodb');
 
 const app = express()
 const port = 8080;
-
 app.use(cors())
 app.use(express.json())
 
+let userCollection;
+
+connectDB().then(collection => {
+ userCollection = collection;
+});
 
 
 app.get('/api/healthcheck', (req, res) => {
   res.status(201).send("It's working");
 })
 
+
+//CRUD
 //Created-POST
 app.post('/users', async (req, res) => {
-  console.log(req.body);
   try {
-    const { nombre, email, password } = req.body
+    const newUser = req.body;
+    await userCollection.insertOne(newUser);
 
-    if (!nombre || !email || !password) {
-      return res.status(400).json({ message: "Todos los campos son obligatorios" });
-    }
-    
-    const newUser = await db.addUser(nombre, email, password)
-    res.status(201).json({message: "User created", data: result})
-
-    console.log(newUser)
-
+    res.status(201).json({message: "User created", data: newUser});
+    console.log(newUser)  
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).json({ error: 'Error al crear usuario' });
   }
 });
 
 
 //Read- GET
-// app.get('/users', (req, res) => {
-//   try{
-//     const response = db.findAll();
-//     console.log(response);
-//     res.status(201).json({message: "Users found", data: response})
-//   } catch (error) { 
-//     res.status(400).json({message: error.message})
-//   }
-// })
+app.get('/users', async (req, res) => {
+  try{
+    const response = await userCollection.find({}).toArray();
+    res.status(201).json({message: "Users found", data: response})
+    console.log(response);
+  } catch (error) { 
+    res.status(400).json({message: error.message})
+  }
+})
 
+//Read by ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
 
+     if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'ID no válido' });//verifica si es un Obj
+    }
+
+    const user = await userCollection.findOne({ _id: new ObjectId(id) });
+
+    if (user) {
+      res.status(201).json({message: "User found", data: user});
+    }
+  } catch (error) {
+    res.status(400).json({ error: 'User not found' });
+  }
+});
+
+//Update-PUT
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = req.body;
+    const result = await userCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedUser }
+    );
+    res.status(201).json({message: "User updated", data: updatedUser });
+  } catch (error) {
+    res.status(400).json({ error: 'Error al actualizar el usuario' });
+  }
+});
+
+//Datele 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userDeleted = req.params
+    userCollection.deleteOne({ _id: new ObjectId(id) });
+    res.status(201).json({message: "User deleted", data: userDeleted });
+
+  } catch (error) {
+    res.status(400).json({ error: 'Error al eliminar el usuario'});
+  }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+  });
