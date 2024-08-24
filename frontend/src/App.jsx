5d2b829d6010b96cd84d0995ea3a911b29dabd43
PVARGASM1 <andrea.vargas18@hotmail.es>
2024-08-23 22:05:01 -0500

    Add header in home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from '../src/components/index'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <h1>Guardian Security</h1>
+    </>
+  )
+}
+
+export default App
